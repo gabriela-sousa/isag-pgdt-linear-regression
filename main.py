3a910d70efa098ebec0483fd740e7ff1daa0fd80
Gabriela Sousa <gabriela@Gabrielas-Air.lan>
2021-11-24 18:39:43 +0000

    clean code

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-ds = pd.read_csv('bodyPerformance.csv')
+def get_input(file_name):
+    return pd.read_csv(file_name)
 
-def get_line(x, y, i = 1000):
-    slope  = 0
-    intercept = 0
+dataset = get_input("bodyPerformance.csv")
+x = dataset.weight_kg
+y = dataset.height_cm
 
-    learning_rate = 0.0001
-    iterations = i
-
-    n = float(len(x))
-
-    for i in range(iterations):
-        y_prediction = slope * x + intercept
-        derivate_slope = (-2/n) * sum(x *(y - y_prediction))
-        derivate_intercept = (-2/n) * (y - y_prediction)
-        slope = slope - learning_rate * derivate_slope
-        intercept = intercept - learning_rate * derivate_intercept
+plt.scatter(x,y)
 
 
+slope = 0
+intercept = 0
+learning_rate = 0.00001
+
+iterations = 1_000
+
+number_elements_x = float(len(x))
+
+for i in range(iterations):
     y_prediction = slope * x + intercept
+    derivate_slope = (-2 / number_elements_x) * sum(x * (y - y_prediction))
+    derivate_intercept = (-2 / number_elements_x) * (y - y_prediction)
+    slope = slope - learning_rate * derivate_slope
+    intercept = intercept - learning_rate * derivate_intercept
+    print(slope, intercept)
 
-    plt.scatter(x,y)
-    plt.plot([min(x), max(x)], [min(y_prediction), max(y_prediction)], color='red')
-    plt.show()
 
+y_prediction = slope * x + intercept
+plt.plot([min(x), max(x)], [min(y_prediction), max(y_prediction)], color='red')
 
-
-
-get_line(ds.weight_kg, ds.height_cm, 500)
+plt.show()
