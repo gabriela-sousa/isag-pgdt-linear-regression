40e88a203e375cf1dd60e4002226b75854509863
Gabriela Sousa <gabriela@Gabrielas-Air.lan>
2021-11-25 11:07:57 +0000

    trying new algorithm

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def get_input(file_name):
-    return pd.read_csv(file_name)
+ds = pd.read_csv('bodyPerformance.csv')
 
-dataset = get_input("bodyPerformance.csv")
-x = dataset.weight_kg
-y = dataset.height_cm
+def error_function(x, y, slope, intercept):
+    '''Defining error function'''
+    n = float(len(x))
+    total_error = 0.0
+    for i in range(n):
+        total_error += (y[i] - (slope * x[i] + intercept))**2
+    return total_error / n
 
-plt.scatter(x,y)
 
+def get_line(x, y, i = 1000):
+    '''getting the regression line'''
+    slope  = 3          #Como fazer a ligação entre este slope e o definido na função error_function
+    intercept = 7       # "" p/ intercept
 
-slope = 0
-intercept = 0
-learning_rate = 0.00001
+    learning_rate = 0.0001
+    iterations = i
 
-iterations = 1_000
+    n = float(len(x))
 
-number_elements_x = float(len(x))
+    for i in range(iterations):
+        y_prediction = slope * x + intercept
+        derivate_slope = (-2/n) * sum(x *(y - y_prediction))
+        derivate_intercept = (-2/n) * (y - y_prediction)
+        slope = slope - learning_rate * derivate_slope
+        intercept = intercept - learning_rate * derivate_intercept
 
-for i in range(iterations):
     y_prediction = slope * x + intercept
-    derivate_slope = (-2 / number_elements_x) * sum(x * (y - y_prediction))
-    derivate_intercept = (-2 / number_elements_x) * (y - y_prediction)
-    slope = slope - learning_rate * derivate_slope
-    intercept = intercept - learning_rate * derivate_intercept
-    print(slope, intercept)
+
+    plt.scatter(x,y)
+    plt.plot([min(x), max(x)], [min(y_prediction), max(y_prediction)], color='red')
 
 
-y_prediction = slope * x + intercept
-plt.plot([min(x), max(x)], [min(y_prediction), max(y_prediction)], color='red')
+def predict_value(x, y, value_x):
+    '''Returning predicted dependent variable value for given independent variable value'''
+    x_mean = x.mean()
+
+    ds['diff_x'] = x_mean - x             #creating new column and getting differences
+
+    ds['diff_x_squared'] = ds.diff_x ** 2 #squaring the differences
+    ssxx = ds.diff_x_squared.sum()
+
+    y_mean = y.mean()
+    ds['diff_y'] = y_mean - y
+
+    ssxy = (ds.diff_x*ds.diff_y).sum()
+
+    slope = ssxy/ssxx
+
+    intercept = y_mean - slope * x_mean
+
+    predict = slope * value_x + intercept
+    plt.plot(value_x, predict, 'go')
+
+get_line(ds.weight_kg, ds.height_cm)
+predict_value(ds.weight_kg, ds.height_cm, range(50, 85, 2))
+
 
 plt.show()
+
+
